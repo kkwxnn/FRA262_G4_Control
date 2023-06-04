07e46956fa3ac991e5c6c687b2b36a121462dfcf
Monsicha Sopitlaptana <122891621+kkwxnn@users.noreply.github.com>
2023-06-04 11:22:07 +0700

    V17
    - QEI Read Position
    - Motor Drive Control (PWM, Direction)
    - Cascade PID (Position, Velocity)
    - Joystick (FSM Button, Tray position, Fine, Rough, Analog)
    - Get Hole Position (new)
    - Trajectory
    - Scheduler
    - Proximity **Close
    - I2C and End Effector Switch
    - Homing (Not finish)
    - Include ModbusRTU
    - ModbusRTU (JoyStick, Go Pick, Go Place, End Effector, Routine)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -821,18 +821,6 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_4, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PC1 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PC2 PC3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
   /*Configure GPIO pins : PA5 PA10 */
   GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
@@ -851,13 +839,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI3_IRQn);
 
 }
 
